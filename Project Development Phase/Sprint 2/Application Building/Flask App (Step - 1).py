5b7d86029f92b74c31a033393308a1493c6aec5f
Manoj26github <92574796+Manoj26github@users.noreply.github.com>
2022-11-18 00:02:00 +0530

    Delete Project Development Phase/Sprint 2 directory

--- Project Development Phase/Sprint 2/Application Building/Flask App (Step - 1).py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import pickle
-#importing the inputScript file used to analyze the URL
-
-
-#load model
-app = Flask(__name__)
-model = pickle.load(open('Phishing_Website.pkl', 'rb'))
